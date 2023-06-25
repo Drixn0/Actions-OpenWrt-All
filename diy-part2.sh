fe094461f7ece3f58a79757ff87fe92f11ae1d0b
Drixn <i@drixn.com>
2023-06-25 10:41:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    
+
+# Set DISTRIB_REVISION
+sed -i "s/OpenWrt_x86_64 /Build $(TZ=UTC-8 date "+%Y.%m.%d") Compiled by Drixn /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.2）
+echo '自定义 LAN 口 IP...'
+sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
+
+# Modify system hostname（FROM OpenWrt CHANGE TO OpenWrt-x86_64）
+sed -i 's/OpenWrt/OpenWrt-x86_64/g' package/base-files/files/bin/config_generate
